89491841ff2a734cc9d39d69ced56f28f759abb5
20kdc <asdd2808@gmail.com>
2021-11-26 12:23:53 +0000

    No server gc (#12)
    Co-authored-by: Pieter-Jan Briers <pieterjan.briers@gmail.com>

--- SS14.Watchdog/Startup.cs
+++ SS14.Watchdog/Startup.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Runtime;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -49,6 +50,8 @@ namespace SS14.Watchdog
             }
 
             app.UseSerilogRequestLogging();
+
+            Log.ForContext<Startup>().Debug($"Using server GC: {GCSettings.IsServerGC}");
 
             // app.UseHttpsRedirection();
 
