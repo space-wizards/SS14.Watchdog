6e4f4910c25eb06904ef58a868e48f83d611d40f
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-07-04 17:42:16 +0200

    Add nix flake, package and module.

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description =
+    "Flake providing a package and NixOS module for the Space Station 14 Watchdog.";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+
+  outputs = { self, nixpkgs }:
+    let
+      nixos-lib = import (nixpkgs + "/nixos/lib") { };
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ]
+          (system: function (import nixpkgs { inherit system; }));
+    in
+    {
+
+      packages = forAllSystems (pkgs: {
+        default = self.packages.${pkgs.system}.space-station-14-watchdog;
+        space-station-14-watchdog =
+          pkgs.callPackage ./nix/package.nix { flake = self; };
+        vm-test = nixos-lib.runTest {
+            name = "space-station-14-watchdog";
+            imports = [ ./nix/test.nix  ];
+            hostPkgs = pkgs;
+            extraBaseModules = { imports = [ self.nixosModules.space-station-14-watchdog ]; };
+        };
+      });
+
+      overlays = {
+        default = self.overlays.space-station-14-watchdog;
+        space-station-14-watchdog = final: prev: {
+          space-station-14-watchdog =
+            self.packages.${prev.system}.space-station-14-watchdog;
+        };
+      };
+
+      apps = forAllSystems (pkgs: {
+        default = self.apps.${pkgs.system}.space-station-14-watchdog;
+        space-station-14-watchdog = {
+          type = "app";
+          program = "${
+              self.packages.${pkgs.system}.space-station-14-watchdog
+            }/bin/SS14.Watchdog";
+        };
+        fetch-deps = {
+          type = "app";
+          program = toString
+            self.packages.${pkgs.system}.space-station-14-watchdog.passthru.fetch-deps;
+        };
+        vm-test = {
+          type = "app";
+          program = "${self.packages.${pkgs.system}.vm-test}/bin/nixos-test-driver";
+        };
+        vm-test-interactive = {
+          type = "app";
+          program = "${self.packages.${pkgs.system}.vm-test.driverInteractive}/bin/nixos-test-driver";
+        };
+      });
+
+      nixosModules = {
+        space-station-14-watchdog = import ./nix/module.nix self;
+        default = self.nixosModules.space-station-14-watchdog;
+      };
+
+      formatter = forAllSystems (pkgs: pkgs.nixpkgs-fmt);
+
+    };
+}
---
+++ nix/test.nix
@@ -0,0 +1,30 @@
+{
+  nodes.machine = { ... }:
+  {
+    services.space-station-14-watchdog = {
+      enable = true;
+      openApiFirewall = true;
+      settings.Servers.Instances.Test = {
+        Name = "Test Instance";
+        ApiToken = "1234";
+        ApiPort = 1212;
+        TimeoutSeconds = 240;
+        UpdateType = "Manifest";
+        Updates.ManifestUrl = "https://central.spacestation14.io/builds/wizards/manifest.json";
+      };
+      instances.Test.configuration = {
+        status = {
+          enabled = true;
+          bind = "*:1212";
+        };
+      };
+    };
+  };
+
+  testScript = ''
+    machine.start()
+    machine.wait_for_unit("space-station-14-watchdog.service")
+    machine.wait_for_open_port(1212)
+    machine.wait_until_succeeds("curl http://127.0.0.1:1212/status");
+  '';
+}
