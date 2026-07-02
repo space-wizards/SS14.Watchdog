4f8b041b29be16db350ddbe7a40d64306d03f08d
metalgearsloth <comedian_vs_clown@hotmail.com>
2026-07-02 13:06:49 +1000

    Add watchdog API auth and ping tests

--- SS14.Watchdog.Tests/Controllers/ServerApiControllerTest.cs
+++ SS14.Watchdog.Tests/Controllers/ServerApiControllerTest.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
 using SS14.Watchdog.Components.ServerManagement;
@@ -56,9 +58,61 @@ namespace SS14.Watchdog.Tests.Controllers
         }
 
         [Test]
-        public void TestPing()
+        public void TestAuthorizationMissingHeader()
         {
+            // Arrange
+            var manager = Mock.Of<IServerManager>();
+            var controller = new ServerApiController(manager);
 
+            // Act
+            var success = controller.TryAuthorize(null!, "honk", out var failure, out var instance);
+
+            // Assert
+            Assert.That(success, Is.False);
+            Assert.That(failure, Is.InstanceOf<UnauthorizedResult>());
+            Assert.That(instance, Is.Null);
+        }
+
+        [Test]
+        public void TestAuthorizationInvalidBase64()
+        {
+            // Arrange
+            var manager = Mock.Of<IServerManager>();
+            var controller = new ServerApiController(manager);
+
+            // Act
+            var success = controller.TryAuthorize("Basic definitely not base64", "honk", out var failure, out var instance);
+
+            // Assert
+            Assert.That(success, Is.False);
+            Assert.That(failure, Is.InstanceOf<BadRequestResult>());
+            Assert.That(instance, Is.Null);
+        }
+
+        [Test]
+        public async Task TestPing()
+        {
+            // Arrange
+            const string secret = "secure that disk";
+            const string key = "foo";
+
+            var instanceMock = new Mock<IServerInstance>();
+            instanceMock.SetupGet(p => p.Secret).Returns(secret);
+            instanceMock.SetupGet(p => p.Key).Returns(key);
+
+            var instance = instanceMock.Object;
+            var manager = new Mock<IServerManager>();
+            manager.Setup(i => i.TryGetInstance(key, out instance)).Returns(true);
+
+            var controller = new ServerApiController(manager.Object);
+            var authCorrect = "Basic " + Convert.ToBase64String(Encoding.UTF8.GetBytes($"{key}:{secret}"));
+
+            // Act
+            var response = await controller.PingAsync(authCorrect, key);
+
+            // Assert
+            Assert.That(response, Is.InstanceOf<OkResult>());
+            instanceMock.Verify(i => i.PingReceived(), Times.Once);
         }
     }
 }
