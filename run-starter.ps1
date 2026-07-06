112cff0ac62e4377a72db6dade59768c87510683
metalgearsloth <comedian_vs_clown@hotmail.com>
2026-07-06 17:47:25 +1000

    weh

--- run-starter.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = "Stop"
-$env:ASPNETCORE_ENVIRONMENT = "Starter"
-$env:DOTNET_ENVIRONMENT = "Starter"
-dotnet run --no-launch-profile --project SS14.Watchdog/SS14.Watchdog.csproj
